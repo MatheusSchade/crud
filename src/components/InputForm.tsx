1601d0b6b0b5885d9c64fd039af2cfd012fe1a24
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2022-07-10 18:17:15 -0300

    ajuste input data nascimeto

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -17,7 +17,18 @@ const InputText: React.FC<InputTextTp> = ({ size, label, change, value, name, ty
         onBlur={blur}
         value={value}
         onChange={change}
-        className={`bg-white text-gray-400 outline-none shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline`}
+        className={
+          `bg-white
+           text-gray-400
+            outline-none
+             shadow
+               border
+                rounded w-full
+                 py-2 px-3
+                  leading-tight
+                   focus:outline-none
+                    focus:shadow-outline
+                    `}
         id={name} name={name} type={type} placeholder={placeholder} />
     </div>
   )
--- src/services/editPatient.tsx
+++ src/services/editPatient.tsx
@@ -4,13 +4,7 @@ import { Form } from "../types/Form"
 import ZipCode from "../types/ZipCode"
 
 export const editPatient = async (form: Form, id: string, zipCodeData: ZipCode) => {
-  // const name: string = newFormToEdit?.name || form?.name
-  // const birthdate: string = newFormToEdit?.birthdate || form?.birthdate
-  // const email: string = newFormToEdit?.email || form?.email
-  // const zipCode: string = newFormToEdit?.zipCode || form?.zipCode
   const address: string = zipCodeData?.logradouro || form?.address
-  // const numberAddress: string = newFormToEdit?.numberAddress || form?.numberAddress
-  // const complement: string = newFormToEdit?.complement || form?.complement
   const neighborhood: string = zipCodeData?.bairro || form?.neighborhood
   const city: string = zipCodeData?.localidade || form?.city
   const state: string = zipCodeData?.uf || form?.state
